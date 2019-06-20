0c31118aa4fd481f2224cc1efb91a7f2db2b9a04
Marcello Paiva <marcellopaiva@Marcellos-MacBook-Pro.local>
2019-06-20 16:09:44 -0400

    Removed Mui aliases and added MuiSwitches to storybook

--- packages/franklin-thor-playground/.storybook/config.js
+++ packages/franklin-thor-playground/.storybook/config.js
@@ -4,16 +4,17 @@ import { withInfo } from '@storybook/addon-info'
 import '@storybook/addon-console'
 
 function loadStories() {
-  require('stories/Button')
-  require('stories/Showcase')
-  require('stories/Checkboxes')
-  require('stories/Selects')
-  require('stories/Switches')
-  require('stories/TextFields')
-  require('stories/MuiButton')
-  require('stories/MuiCheckboxes')
-  require('stories/MuiPickers')
-  require('stories/MuiSelects')
+  //require('stories/Button')
+  //require('stories/Showcase')
+  //require('stories/Checkboxes')
+  //require('stories/Selects')
+  //require('stories/Switches')
+  //require('stories/TextFields')
+  //require('stories/MuiButton')
+  //require('stories/MuiCheckboxes')
+  //require('stories/MuiPickers')
+  require('stories/MuiSwitches')
+  //require('stories/MuiSelects')
 }
 
 addDecorator(withKnobs)
---
+++ packages/franklin-thor-playground/src/stories/MuiSwitches/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+// import { boolean, text, select } from '@storybook/addon-knobs';
+import CustomizedSwitches from '../../page/MuiSwitches/CustomSwitches'
+
+storiesOf('MuiSwitches', module).add('Custom Switches', () => <CustomizedSwitches />)
