c5a9467a34014241c0b9a3a278ba5bcad9dc72f6
Marcello Paiva <marcellopaiva@Marcellos-MacBook-Pro.local>
2019-07-22 15:49:16 -0400

    worked on styling custom FTITable

--- services/franklin-thor-playground/src/stories/FTITable/index.js
+++ services/franklin-thor-playground/src/stories/FTITable/index.js
@@ -2,5 +2,11 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 // import { boolean, text, select } from '@storybook/addon-knobs';
 import FTITable from '../../page/FTIPatterns/FTITable/FTITable'
+import { ThemeProvider } from '@material-ui/styles'
+import theme from '../../themes/default'
 
-storiesOf('FTITable', module).add('FTI Table', () => <FTITable />)
+theme.then(apiTheme =>
+  storiesOf('FTITable', module)
+    .addDecorator(getStory => <ThemeProvider theme={apiTheme}>{getStory()}</ThemeProvider>)
+    .add('FTI Table', () => <FTITable />),
+)
